0293fc1cc5324381a1150e119bcaff752f3a3e9f
unknown <mietlinski.wojtek@gmail.com>
2020-07-26 22:46:58 +0200

    Updates

---
+++ precache-manifest.10d40002123035e933d51b48c098eecd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b2c2e52d119ac1c0ad24a7f684842b9",
+    "url": "/foodie/index.html"
+  },
+  {
+    "revision": "00b422deebb2ab2a4026",
+    "url": "/foodie/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "42ce2b426d4952f5131e",
+    "url": "/foodie/static/js/2.4a1cdcc2.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/foodie/static/js/2.4a1cdcc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00b422deebb2ab2a4026",
+    "url": "/foodie/static/js/main.7e5fb92a.chunk.js"
+  },
+  {
+    "revision": "f3e583f8f6f500aca603",
+    "url": "/foodie/static/js/runtime-main.b5a0ae38.js"
+  }
+]);
